343ddf28ed35b3f3f83ea9d905ab025fab434d84
bragelbytes <joebragel@gmail.com>
2025-06-09 18:57:06 -0700

    Jumping Animations

--- Assets/MoveController.cs
+++ Assets/MoveController.cs
@@ -39,8 +39,12 @@ public class MoveController : MonoBehaviour
 
     private void AnimationControllers()
     {
-        bool isMoving = rb.linearVelocityX != 0;
-        anim.SetBool("isMoving", isMoving);
+        // bool isMoving = rb.linearVelocityX != 0;
+        // anim.SetBool("isMoving", isMoving);
+
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void CollisionChecks()
